e40dda3bbf441489e32272024093e1f4bbc2712a
chaszg <46233328+chaszg@users.noreply.github.com>
2019-07-24 14:45:11 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(name='comp_alchemy',
+                  version=0.0.1,
+                  author='Charles Griego',
+                  packages=['comp_alchemy'],
+                  python_requires='>=3',
+                  install_requires=['ase'])
+                  
