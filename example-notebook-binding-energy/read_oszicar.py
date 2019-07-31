ea06536339cd8c3d0589f10eb079112ccb076418
Chasz <charles.griego18@gmail.com>
2019-07-31 13:02:59 -0400

    Cleaning up notebook directory

--- example-notebook-binding-energy/read_oszicar.py
+++
@@ -1,7 +0,0 @@
-#!/bin/env python
-
-def grab_energy(oszicar):
-    '''Grabs energy (E0) from OSZICAR file.'''
-    with open(oszicar, 'r') as os:
-        energy = float(os.readlines()[-1].split()[4])
-    return energy
